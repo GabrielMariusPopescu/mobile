b26ddbd2c8c3f268f12ac6323c18495218dc149e
Gabriel Marius Popescu <GabyMarius@hotmail.com>
2021-01-17 22:38:41 +0000

    first app

---
+++ TestSetup/TestSetup/TestSetup/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TestSetup.Services;
+using TestSetup.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestSetup
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TestSetup/TestSetup/TestSetup/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestSetup.Models;
+using TestSetup.ViewModels;
+using TestSetup.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestSetup.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
