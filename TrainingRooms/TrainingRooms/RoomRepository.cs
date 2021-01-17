3eb53c6afe3fe568becd0ae670cf7d64856790bf
Gabriel Marius Popescu <GabyMarius@hotmail.com>
2021-01-17 23:53:18 +0000

    recreated the project from the lesson

--- TrainingRooms/TrainingRooms/RoomRepository.cs
+++ TrainingRooms/TrainingRooms/RoomRepository.cs
@@ -1,44 +1,48 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace TrainingRooms
 {
     public class RoomRepository
     {
+        private List<TrainingRoom> _rooms =
+            new List<TrainingRoom>
+            {
+                new TrainingRoom{
+                    Id = 1,
+                    Name = "Copernicus",
+                    Location = "bldg 1",
+                    NumberComputers = 25
+                },
+                new TrainingRoom{
+                    Id = 2,
+                    Name = "Sagittarius",
+                    Location = "bldg 1",
+                    NumberComputers = 10
+                },
+                new TrainingRoom{
+                    Id = 3,
+                    Name = "Luna",
+                    Location = "bldg 3",
+                    NumberComputers = 50
+                }
+
+            };
         public RoomRepository()
         {
-
         }
 
-        public List<TrainingRoom> GetRooms() => trainingRooms;
-
-        public TrainingRoom GetRoom(int id) => trainingRooms.FirstOrDefault(it => it.Id == id);
-
-        //
-
-        private readonly List<TrainingRoom> trainingRooms = new List<TrainingRoom>
+        public List<TrainingRoom> GetRooms()
         {
-            new TrainingRoom
-            {
-                Id = 1,
-                Name = "Copernicus",
-                Location = "Building 1",
-                NumberOfComputers = 25,
-            },
-            new TrainingRoom
-            {
-                Id = 2,
-                Name = "Sagittarius",
-                Location = "Building 1",
-                NumberOfComputers = 20,
-            },
-            new TrainingRoom
-            {
-                Id = 2,
-                Name = "Luna",
-                Location = "Building 3",
-                NumberOfComputers = 50,
-            }
-        };
+            return _rooms;
+        }
+
+        public TrainingRoom GetRoom(int id)
+        {
+            return (from r in _rooms
+            where r.Id == id
+             select r).FirstOrDefault();
+        }
     }
 }
--- TrainingRooms/TrainingRooms/TrainingRoom.cs
+++ TrainingRooms/TrainingRooms/TrainingRoom.cs
@@ -1,13 +1,30 @@
-﻿namespace TrainingRooms
+﻿using System;
+
+namespace TrainingRooms
 {
     public class TrainingRoom
     {
-        public int Id { get; set; }
+        public int Id
+        {
+            get;
+            set;
+        }
 
-        public string Name { get; set; }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Location
+        {
+            get;
+            set;
+        }
 
-        public string Location { get; set; }
-
-        public int NumberOfComputers { get; set; }
+        public int NumberComputers
+        {
+            get;
+            set;
+        }
     }
 }
