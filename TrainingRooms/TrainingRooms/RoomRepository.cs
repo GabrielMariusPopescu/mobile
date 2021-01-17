bfdf0702b7501426a7fb3418b796da4329edae18
Gabriel Marius Popescu <GabyMarius@hotmail.com>
2021-01-17 23:37:19 +0000

    added android project

---
+++ TrainingRooms/TrainingRooms/RoomRepository.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TrainingRooms
+{
+    public class RoomRepository
+    {
+        public RoomRepository()
+        {
+
+        }
+
+        public List<TrainingRoom> GetRooms() => trainingRooms;
+
+        public TrainingRoom GetRoom(int id) => trainingRooms.FirstOrDefault(it => it.Id == id);
+
+        //
+
+        private readonly List<TrainingRoom> trainingRooms = new List<TrainingRoom>
+        {
+            new TrainingRoom
+            {
+                Id = 1,
+                Name = "Copernicus",
+                Location = "Building 1",
+                NumberOfComputers = 25,
+            },
+            new TrainingRoom
+            {
+                Id = 2,
+                Name = "Sagittarius",
+                Location = "Building 1",
+                NumberOfComputers = 20,
+            },
+            new TrainingRoom
+            {
+                Id = 2,
+                Name = "Luna",
+                Location = "Building 3",
+                NumberOfComputers = 50,
+            }
+        };
+    }
+}
---
+++ TrainingRooms/TrainingRooms/TrainingRoom.cs
@@ -0,0 +1,13 @@
+﻿namespace TrainingRooms
+{
+    public class TrainingRoom
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public int NumberOfComputers { get; set; }
+    }
+}
